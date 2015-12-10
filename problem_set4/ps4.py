d583c9c4775f3234df9aae905d97f5aee62b37fd
alisazzz <alisa.zwy@gmail.com>
2015-12-09 23:21:05 -0500

    ps4 - test cases passed, need to eliminate cases of last sink vertices, and intermediary one-way street nodes

--- problem_set4/ps4.py
+++ problem_set4/ps4.py
@@ -1,74 +1,89 @@
 
-def parse_file(file, reverse=False):
+def reverse_arcs(file):
     arcs = {}
     with open(file) as f:
         for s in f:
-            tail, head = (int(x) for x in s.split(' ')[:2])
-            if reverse:
-                tail, head = head, tail
+            head, tail = (int(x) for x in s.split(' ')[:2])
             if tail in arcs:
                 arcs[tail].append(head)
             else:
                 arcs[tail] = [head]
     return arcs
-    
 
 
 
-def topo_dfs(graph, start, visited, currLabel):
+def scc_dfs(graph, start, visited, currLabel, minions):
     visited[start] = 0
     heads = graph.get(start)
     if heads:
         for head in heads:
             if head not in visited:
-                visited, currLabel = topo_dfs(graph, head, visited, currLabel)
-    visited[start] = currLabel
-    currLabel -= 1
-    return visited, currLabel
-
-
-def topo_order(graph):
-    visited = {}
-    currLabel = int(input("how many nodes: "))
-    for tail in graph:
-        if tail not in visited:
-            visited, currLabel = topo_dfs(graph, tail, visited, currLabel)
-    return visited
-
-
-
-
-def scc_dfs(graph, start, visited, currLabel):
-    visited[start] = 0
-    heads = graph.get(start)
-    if heads:
-        for head in heads:
-            if head not in visited:
-                visited, currLabel = scc_dfs(graph, head, visited, currLabel)
+                minions.append(head)
+                visited, currLabel, minions = scc_dfs(graph, head, visited, currLabel, minions)
     visited[start] = currLabel
     currLabel += 1
-    return visited, currLabel
+    return visited, currLabel, minions
 
 
 def scc_loop(graph, qtyNodes):
     visited = {}
     label = 1
+    leadersList = {}
     for i in range(qtyNodes, 0, -1):
         if i in graph and i not in visited:
-            visited, label = scc_dfs(graph, i, visited, label)
-    return visited, label
+            minions = []
+            visited, label, minions = scc_dfs(graph, i, visited, label, minions)
+            leadersList[i] = minions
+    return visited, leadersList
 
 
-def scc_main(graph):
+def node_to_label(oldGraph, labels):
+    newGraph = {}
+    while oldGraph:
+        head, adjTails = oldGraph.popitem()
+        newTail = labels[head]
+        for head in adjTails:
+            newHead = labels[head]
+            if newHead in newGraph:
+                newGraph[newHead].append(newTail)
+            else:
+                newGraph[newHead] = [newTail]
+    return newGraph
+
+
+def scc_main(file):
     # reverse graph
-    file = input("file name? ") + ".txt"
     qtyNodes = int(input("how many nodes? "))
-    graphRev = parse_file(file, reverse=True)
-    SOMETHING = scc_loop(graphRev, qtyNodes)
+    graphRev = reverse_arcs(file)
     
     # call scc_loop(graphRev)
+    nodesLabelled, leadersList = scc_loop(graphRev, qtyNodes)
+    leadersList = None    # save memory
+    # update graph using nodes' labels
+    newGraph = node_to_label(graphRev, nodesLabelled)
+    """graphRev popitem()-ed in sub-routine, and is now zero in main as well"""
+    nodesLabelled = None
     
     # call scc_loop(graph) with labels changed
+    nodesLabelled, leaders = scc_loop(newGraph, qtyNodes)
+    nodesLabelled = None
+    
+    # dfs on newGraph, start w/ leader, only nodes in its scc would be discovered
+    bigFive = [0, 0, 0, 0, 0]
+    leadersList = []
+    while leaders:
+        leadersList.append(leaders.popitem()[0])
+    leadersList.sort()
+    visited = {}
+    while leadersList:
+        start = leadersList.pop()
+        visited, disregard, scc = scc_dfs(newGraph, start, visited, 0, [])
+        size = len(scc) + 1    # plus the leader itself
+        if size > bigFive[-1]:
+            bigFive.pop()
+            bigFive.append(size)
+            bigFive.sort(reverse=True)
+    return bigFive
 
 
 
