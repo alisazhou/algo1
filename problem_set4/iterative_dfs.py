db5fef3e6c8dbf66d45fbd2656e7f99eda9077f3
alisazzz <alisa.zwy@gmail.com>
2015-12-12 17:35:41 -0500

    fails on testcase3. separately fails in cases where the node numbers are not continuous (for i in range(qtyNodes, 0, -1) does not account for that)

--- problem_set4/iterative_dfs.py
+++ problem_set4/iterative_dfs.py
@@ -14,46 +14,108 @@ def reverse_arcs(file):
 
 def scc_dfs(graph, start, visited, label):
     visited[start] = 0
-    minions = []
+    parent = []
+    children = []
     nodeStack = [start]
+    minions = []
     while nodeStack:
-        print("pre-pop stack is", nodeStack)
         node = nodeStack.pop()
-        print("post-pop stack is", nodeStack)
+        anteStackLen = len(nodeStack)
         try:
-            adjNodes = graph.pop(node)
+            adjNodes = graph[node]
         except KeyError:
-            # node is a sink vertex
+            adjNodes = []
+        parent.append(node)
+        children.append(adjNodes)
+        for w in adjNodes:
+            if w not in visited:
+                visited[w] = 0
+                nodeStack.append(w)
+        # determine if current popped node is fully explored
+        if len(nodeStack) == anteStackLen:
             visited[node] = label
-            print("sink vertex", node, "'s label is", label)
+            minions.append(node)
             label += 1
-            continue
-        for i in adjNodes:
-            if i not in visited:
-                visited[i] = 0
-                minions.append(i)
-                nodeStack.append(i)
-    minions.reverse()
-    print(minions)
-    for nion in minions:
-        visited[nion] = label
-        label += 1
-    visited[start] = label
-    label += 1
-    print("visited for leader", start, "is", visited)
-    return visited, minions, label
+            children.pop()
+            parent.pop()
+            # are all the current siblings fully explored
+            while children:
+                siblings = children.pop()
+                allExplored = True
+                for sib in siblings:
+                    if sib in nodeStack:
+                        allExplored = False
+                        break
+                if not allExplored:
+                    children.append(siblings)
+                    break
+                tail = parent.pop()
+                visited[tail] = label
+                minions.append(tail)
+                label += 1
+    return visited, label, minions
 
-            
-    
+
+
+def node_to_label(oldGraph, labels):
+    newGraph = {}
+    while oldGraph:
+        head, adjTails = oldGraph.popitem()
+        newTail = labels[head]
+        for head in adjTails:
+            newHead = labels[head]
+            if newHead in newGraph:
+                newGraph[newHead].append(newTail)
+            else:
+                newGraph[newHead] = [newTail]
+    return newGraph
 
 def scc_loop(graph, qtyNodes):
     visited = {}
-    leadersList = {}
+    leaders = {}
     label = 1
     for i in range(qtyNodes, 0, -1):
         if i not in visited:
-            print("leader is now", i)
-            visited, minions, label = scc_dfs(graph, i, visited, label)
-            leadersList[i] = minions
-    return visited
+            visited, label, minions = scc_dfs(graph, i, visited, label)
+            leaders[i] = minions
+    return visited, leaders
+
+
+def scc_main(file):
+    qtyNodes = int(input("how many nodes? "))
+    startTime = time.time()
+    graphRev = reverse_arcs(file)
+    print("--- reverse_arcs() took %s seconds ---" % (time.time() - startTime))
+    
+    # call scc_loop(graphRev)
+    startTime = time.time()
+    nodesLabelled, leadersList = scc_loop(graphRev, qtyNodes)
+    print("--- 1st pass took %s seconds ---" % (time.time() - startTime))
+    leadersList = None    # save memory
+
+    # update graph using nodes' labels
+    startTime = time.time()
+    newGraph = node_to_label(graphRev, nodesLabelled)
+    print("--- node_to_label() took %s seconds ---" % (time.time() - startTime))
+    """graphRev popitem()-ed in sub-routine, and is now zero in main as well"""
+    nodesLabelled = None
+    
+    # call scc_loop(graph) with labels changed
+    startTime = time.time()
+    nodesLabelled, leaders = scc_loop(newGraph, qtyNodes)
+    print("--- 2nd pass took %s seconds ---" % (time.time() - startTime))
+    nodesLabelled = None
+    
+    # dfs on newGraph, start w/ leader, only nodes in its scc would be discovered
+    startTime = time.time()
+    bigFive = [0, 0, 0, 0, 0]
+    while leaders:
+        sccSize = len(leaders.popitem()[1])
+        if sccSize > bigFive[-1]:
+            bigFive.pop()
+            bigFive.append(sccSize)
+            bigFive.sort(reverse=True)
+    print("--- comparing scc sizes took %s seconds ---" % (time.time() - startTime))
+    print("answer is:", bigFive)
+    return bigFive
 
