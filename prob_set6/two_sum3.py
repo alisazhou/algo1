f309ef7ff2b5816f00cd55f7372c5fa3ef50ba79
Alisa Zhou <alisa.zwy@gmail.com>
2016-01-08 12:10:21 -0500

    two_sum3 passed all testcases, push to test on work computer

--- prob_set6/two_sum3.py
+++ prob_set6/two_sum3.py
@@ -1,5 +1,5 @@
 import time
-from bisect import bisect_right
+from bisect import bisect_left
 
 
 def sort_ints(file_ints):
@@ -26,6 +26,26 @@ def count_targets(searchList):
     Return count of possible target sums.
     """
     count = 0
+    targets = {}
+    indX = 0
+    for x in searchList:
+        # use binary search to find index of (-10000 - x), lo
+        lo = bisect_left(searchList, -10000 - x)
+        # set index start to the greater of either lo or x's index + 1 (excl x)
+        startInd = max(lo, indX + 1)
+
+        # from start, go up elems in searchList, until elem > 10000 - x
+        for elem in searchList[startInd:]:
+            if elem > 10000 - x:
+                break
+            # check if elem + x is in targets, if not, add to targets
+            target = elem + x
+            if target in targets:
+                continue
+            else:
+                count += 1
+                targets[target] = count
+        indX += 1
 
     return count
 
